549664fc5c8c93e6dd76cb5ad1172f367674b7fe
Sándor Székelyhidi <91669591+sacaaa@users.noreply.github.com>
2024-04-06 21:01:52 +0200

    add: decrypt digital signature with decrypter.py

---
+++ decrypter.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from encrypter import signature_verify, get_message_from_signature
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Verify a digital signature and decrypt the message.')
+    parser.add_argument('signature', type=int, help='The signature to verify.')
+    parser.add_argument('public_key', type=int, help='The public key used for verification.')
+    parser.add_argument('modulus', type=int, help='The modulus used for verification.')
+    args = parser.parse_args()
+
+    message = get_message_from_signature(args.signature, args.public_key, args.modulus)
+
+    if message:
+        verification = signature_verify(message, args.signature, args.public_key, args.modulus)
+
+        print(f'Verification: {verification}\n'
+              f'Decrypted message: {message}')
